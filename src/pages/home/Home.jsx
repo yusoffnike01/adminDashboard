07ef49965cf4317a1bc5f2e58e57971d610da7b9
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-05-09 00:12:45 +0800

    add widget

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import SideBar from '../../components/SideBar/Sidebar';
 import NavBar from '../../components/NavBar/NavBar';
 import './home.scss'
+import Widget from '../../components/widget/widget';
+
 
 const Home =()=>{
 return (<div className='home'>
@@ -9,6 +11,12 @@ return (<div className='home'>
 <div className='homeContainer'>
   <div className='homeContainer'>
     <NavBar/>
+    <div className='widgets'>
+      <Widget type="user"/>
+      <Widget type="order"/>
+      <Widget type="earning"/>
+      <Widget type="balance"/>
+    </div>
   </div>
 </div>
 </div>)
