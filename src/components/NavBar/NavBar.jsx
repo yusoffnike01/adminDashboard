2e8d4b7583075e7a30fc00d811632e3f31407186
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-05-08 23:00:20 +0800

    add navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,47 @@
-import './NavBar.scss'
+import './NavBar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 const NavBar=()=>{
 
-  return (<div>
-    NavBar
+  return (<div className='navbar'>
+   <div className='wrapper'>
+     <div className='search'>
+       <input type="text" placeholder='Search....'/>
+       <SearchIcon/>
+     </div>
+     <div className='items'>
+       <div className='item'>
+         <LanguageOutlinedIcon className='icon'/>
+         English 
+       </div>
+       <div className='item'>
+         <DarkModeOutlinedIcon className='icon'/>
+       </div>
+       <div className='item'>
+         <FullscreenExitOutlinedIcon className='icon'/>
+       </div>
+       <div className='item'>
+         <NotificationsNoneOutlinedIcon className='icon'/>
+         <div className='counter'>2</div>
+       </div>
+       <div className='item'>
+         <ChatBubbleOutlineOutlinedIcon className='icon'/>
+         <div className='counter'>5</div>
+       </div>
+       <div className='item'>
+         <ListOutlinedIcon className='icon'/>
+       </div>
+       <div className='item'>
+         <img className='avatar'></img>
+       </div>
+     </div>
+   </div>
   </div>)
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import SideBar from '../../components/SideBar/Sidebar';
+import NavBar from '../../components/NavBar/NavBar';
 import './home.scss'
 
 const Home =()=>{
 return (<div className='home'>
 <SideBar/>
-<div className='homeContainer'>container</div>
+<div className='homeContainer'>
+  <div className='homeContainer'>
+    <NavBar/>
+  </div>
+</div>
 </div>)
 }
 
