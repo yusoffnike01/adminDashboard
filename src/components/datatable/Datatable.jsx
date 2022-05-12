75ef887630defb39fd8f8820056ae26204f8f866
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-05-12 17:00:17 +0800

    update

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,9 +2,15 @@ import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../../datatablesource';
 import './datatable.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Datatable=()=>{
 
+  const [data, setData] = useState(userRows);
+
+  const handleDelete =(id)=>{
+    setData(data.filter((item)=>item.id!==id));
+  }
   const actionColumn =[
     {field: "action", headerName: "Action", width:360, renderCell:()=>{
       return(
@@ -12,7 +18,7 @@ const Datatable=()=>{
            <Link to="/users/test" style={{textDecoration: "none"}}>    
           <div className='viewButton'>View</div>
           </Link>
-          <div className='deleteButton'>Delete</div>
+          <div className='deleteButton' onClick={()=> handleDelete(params.row.id)}>Delete</div>
         </div>
       )
     }}
